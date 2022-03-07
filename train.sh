0c2dc22f44d4a5037161b94b416220d4e7f043cf
Chin-Tang Chen <chintang@cml12.csie.ntu.edu.tw>
2022-03-08 04:44:42 +0800

    update train script

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@
 # CUDA_VISIBLE_DEVICES=2 python run_nerf.py --config configs/chair.txt --finest_res 1024 --lrate 0.01 --lrate_decay 100
 # CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/chair.txt --finest_res 1024 --log2_hashmap_size 14 
 
-CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/mutant.txt --finest_res 1024 
+CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/mutant.txt --finest_res 1024
 CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/bouncingballs.txt --finest_res 1024 
 CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/lego.txt --finest_res 1024 
+CUDA_VISIBLE_DEVICES=0 python run_nerf.py --config configs/lego.txt --finest_res 1024 --I_embed 0 --i_embed_views 0 --i_video 10000
